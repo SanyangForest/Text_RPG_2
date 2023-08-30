ec11bf991b3cfe2815c6cb26c2f25c2f026a22e5
SanyangForest <84529828+SanyangForest@users.noreply.github.com>
2023-08-30 10:13:21 +0900

    작업한것 복붙
    작업한 것 복붙

---
+++ Team_Text_RPG/Team_Text_RPG/Dungeon.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Text_Rpg
+{
+    internal class Dungeon
+    {
+
+
+
+        Character player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+
+        internal void ChoiceDungeon()
+        {
+
+
+            Console.Clear();
+            Console.WriteLine("1. 마을");
+            Console.WriteLine("2. 배틀");
+            Console.WriteLine("진입하시겠습니까?");
+
+            int input = Program.CheckValidInput(1, 2);
+            switch (input)
+            {
+                case 1:
+                    Program.DisplayGameIntro();
+                    break;
+                case 2:
+                    Battle();
+                    break;
+            }
+
+
+        }
+
+        public void Battle()
+        {
+            Monster[] monsters = {
+            new Monster("대포미니언", 150, 150, 20,2),
+            new Monster("미니언", 100, 100, 10,1),
+            new Monster("공허충", 3, 200, 15,3)
+        };
+
+            Random random = new Random();
+            int randomIndex = random.Next(monsters.Length);
+            Monster randomMonster = monsters[randomIndex];
+
+            Console.WriteLine($"{randomMonster.Name}가 등장했다!");
+            do
+            {
+
+                Console.Clear();
+                randomMonster.StatusRender(randomMonster.Name);
+
+
+
+
+
+                Console.WriteLine("1. 때린다");
+                Console.WriteLine("2. 스킬");
+                Console.WriteLine("3. 도망간다");
+                int input = Program.CheckValidInput(1, 3);
+                switch (input)
+                {
+                    case 1:
+                        player.BattleLogic(randomMonster);
+                        break;
+                    case 2:
+                        // 스킬
+                        break;
+                    case 3:
+                        Program.DisplayGameIntro();
+                        break;
+                }
+
+                Console.ReadLine();
+            } while (true);
+        }
+
+        enum STARTSELECT
+        {
+            SLELCTTOWN,
+            SLELCBETTLE,
+            NONSELECT
+
+        }
+
+
+
+
+
+    }
+
+    public class FightUnit
+    {
+        public string Name;
+        public int Level { get; set; }
+
+        public int Def { get; set; }
+        protected int MaxHp { get; set; }
+        public int Hp { get; set; }
+
+
+        public int Atk { get; set; }
+
+        public int Gold { get; set; }
+
+        public void StatusRender(string _name)
+        {
+            Name = _name;
+            Console.Write(Name);
+            Console.WriteLine("의능력치-----------------------------------------------");
+            Console.Write("공격력:");
+            Console.WriteLine(Atk);
+            //50/100
+            Console.Write("체력:");
+            Console.Write(Hp);
+            Console.Write("/");
+            Console.WriteLine(MaxHp);
+            Console.WriteLine("-----------------------------------------------");
+        }
+        public void BattleLogic(Monster monster)
+        {
+
+
+            Console.WriteLine($"Lv.{Level} {Name} 의 공격!");
+            Console.WriteLine($" {Name} 을(를) 맞췄습니다.  [데미지: {Atk}]");
+
+            Console.WriteLine($"Lv.{Level} {Name}");
+            Console.WriteLine($"HP{Hp}-> {Hp -= Atk}");
+            Console.WriteLine($"HP {Hp}");
+
+
+        }
+
+
+    }
+
+
+
+    public class Monster : FightUnit
+    {
+
+        public Monster(string Name, int level, int atk, int hp, int gold)
+        {
+
+
+            Level = level;
+            Atk = atk;
+
+            Hp = hp;
+            Gold = gold;
+        }
+    }
+}
---
+++ Team_Text_RPG/Team_Text_RPG/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using Text_Rpg;
+internal class Program //커밋 해보기
+
+{
+
+    internal static Character player;
+    internal static Inventory myinventory;
+    internal static Equipment equipmentitem;
+
+    static void Main(string[] args)
+    {
+        GameDataSetting();
+        DisplayGameIntro();
+    }
+
+    static void GameDataSetting()
+    {
+        // 캐릭터 정보 세팅
+        player = new Character("Chad", "전사1", 1, 10, 5, 100, 1500);
+
+        // 인벤토리 정보 세팅
+        myinventory = new Inventory();
+        equipmentitem = new Equipment(0, 0, 0);
+
+        // 아이템 정보 세팅
+    }
+
+    internal static void DisplayGameIntro()
+    {
+        Dungeon dungeon = new Dungeon();
+
+        Console.Clear();
+
+        Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+        Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
+        Console.WriteLine();
+        Console.WriteLine("1. 상태보기");
+        Console.WriteLine("2. 인벤토리");
+        Console.WriteLine("3. 던전");
+        Console.WriteLine();
+        Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+        int input = CheckValidInput(1, 3);
+        switch (input)
+        {
+            case 1:
+                DisplayMyInfo();
+                break;
+
+            case 2:
+                DisplayInventory();
+                break;
+            case 3:
+                dungeon.ChoiceDungeon();
+                break;
+        }
+    }
+    static void Shop()
+    {
+        Console.Clear();
+        Console.WriteLine("상점에 오신 것을 환영합니다 !");
+        Console.WriteLine("아래에 물건을 구매하실 수 있습니다.");
+        Console.WriteLine("현재 소지액: " + player.Gold);
+
+    }
+    static void DisplayMyInfo()
+    {
+        Console.Clear();
+
+        Console.WriteLine("상태보기");
+        Console.WriteLine("캐릭터의 정보르 표시합니다.");
+        Console.WriteLine();
+        Console.WriteLine($"Lv.{player.Level}");
+        Console.WriteLine($"{player.Name}({player.Job})");
+        Console.WriteLine($"공격력 :{player.Atk}");
+        Console.WriteLine($"방어력 : {player.Def}");
+        Console.WriteLine($"체력 : {player.Hp}");
+        Console.WriteLine($"Gold : {player.Gold} G");
+        Console.WriteLine();
+        Console.WriteLine("0. 나가기");
+
+        int input = CheckValidInput(0, 0);
+        switch (input)
+        {
+            case 0:
+                DisplayGameIntro();
+                break;
+        }
+    }
+
+    static void DisplayInventory()
+    {
+        Console.Clear();
+
+        Console.WriteLine("인벤토리");
+        Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+        Console.WriteLine();
+        Console.WriteLine("[아이템 목록]");
+        Console.WriteLine($"- 1   ");
+        Console.WriteLine($"- 2   ");
+        Console.WriteLine();
+        Console.WriteLine("1. 장착 관리");
+        Console.WriteLine("0. 나가기");
+
+        int input = CheckValidInput(0, 1);
+        switch (input)
+        {
+            case 0:
+                DisplayGameIntro();
+                break;
+
+            case 1:
+                DisplayEquipItem();
+                break;
+        }
+
+        static void DisplayEquipItem()
+        {
+            Console.Clear();
+
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine($"-1 ");
+            Console.WriteLine($"-2 ");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            int input = CheckValidInput(0, 2);
+            switch (input)
+            {
+                case 0:
+                    DisplayInventory();
+                    break;
+
+                case 1:
+                    DisplayEquipItem();
+                    break;
+
+                case 2:
+                    DisplayEquipItem();
+                    break;
+            }
+        }
+    }
+
+    internal static int CheckValidInput(int min, int max)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            bool parseSuccess = int.TryParse(input, out var ret);
+            if (parseSuccess)
+            {
+                if (ret >= min && ret <= max)
+                    return ret;
+            }
+
+            Console.WriteLine("잘못된 입력입니다.");
+        }
+    }
+
+    public class Equipment
+    {
+        public List<Item> EquipItems = new List<Item>();
+        public void EquipingItem(Item item)
+        {
+            EquipItems.Add(item);
+            equipmentitem.AddEquipItem();
+            item.IsEquip = !item.IsEquip;
+        }
+        public void UnEquipingItem(Item item)
+        {
+            EquipItems.Remove(item);
+
+            item.IsEquip = !item.IsEquip;
+        }
+        public void AddEquipItem()
+        {
+            AddAtk = AddDef = AddHp = 0;
+            for (int i = 0; EquipItems.Count != i; i++)
+            {
+                AddAtk += EquipItems[i].Atk;
+                AddDef += EquipItems[i].Def;
+                AddHp += EquipItems[i].Hp;
+            }
+        }
+        public int AddAtk { get; set; }
+        public int AddDef { get; set; }
+        public int AddHp { get; set; }
+
+        public Equipment(int addatk, int adddef, int addhp)
+        {
+            AddAtk = addatk;
+            AddDef = adddef;
+            AddHp = addhp;
+        }
+    }
+}
+
+public class Item
+{
+    public string Name { get; }
+    public string Info { get; }
+    public int Atk { get; }
+    public int Def { get; }
+    public int Hp { get; }
+    public bool IsEquip { get; set; }
+
+    public Item(string name, string info, int atk, int def, int hp, bool isEquip)
+    {
+        Name = name;
+        Info = info;
+        Atk = atk;
+        Def = def;
+        Hp = hp;
+        IsEquip = isEquip;
+    }
+}
+
+public class Inventory
+{
+    public List<Item> InventoryItem = new List<Item>();
+    public void AddItemInventory(Item item)
+    {
+        InventoryItem.Add(item);
+    }
+    public void RemoveItemInventory(Item item)
+    {
+        InventoryItem.Remove(item);
+    }
+}
+
+public class Character : FightUnit
+{
+    public string Name { get; }
+    public string Job { get; }
+    public int Level { get; }
+    public int Atk { get; }
+    public int Def { get; }
+    public int Hp { get; }
+    public int Gold { get; }
+
+    public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+    {
+        Name = name;
+        Job = job;
+        Level = level;
+        Atk = atk;
+        Def = def;
+        Hp = hp;
+        Gold = gold;
+    }
+}
